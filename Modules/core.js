c377ef5aff50e62a27e180b7f3836bf8dd502138
dzsofri <beretizsofia@turr.hu>
2024-11-26 11:01:15 +0100

    az én részem (legfontosabb)

--- Modules/core.js
+++ Modules/core.js
@@ -3,7 +3,19 @@ const ejs = require('ejs');
 const router = express.Router();
 const db = require('./database');
 const moment = require('moment');
+const { error } = require('console');
+const app = express();
 
 
+router.get('/', (req, res) => {
+    ejs.renderFile('./views/index.ejs', { session: req.session }, (err, html) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        
+        res.send(html);
+    });
+});
 
 module.exports = router;
