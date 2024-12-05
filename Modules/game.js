1222c33b3c9c264eb8d7ad8b4c115013c542a78c
somodikonrad <somodikonrad@turr.hu>
2024-12-05 17:35:57 +0100

    Kérdések megjelenítése

--- Modules/game.js
+++ Modules/game.js
@@ -1,31 +1,55 @@
-const io = require('socket.io')(server);
+require('dotenv').config();
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const mysql = require('mysql');
+const session = require('express-session');
 
-socket.on('updatePlayerList', (players) => {
-    const playerListElement = document.querySelector('#playerList');
-    playerListElement.innerHTML = ''; // Lista ürítése
-    players.forEach(player => {
-        const listItem = document.createElement('li');
-        listItem.textContent = player;
-        playerListElement.appendChild(listItem);
-    });
+// Alap beállítások
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const server = http.createServer(app);
+const io = socketio(server);  // A socket.io inicializálása a szerveren
+
+// MySQL kapcsolat
+const pool = mysql.createPool({
+    connectionLimit: process.env.CONNECTIONLIMIT,
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME
 });
 
+// Csatlakozás ellenőrzése
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.log('Hiba a MySQL kapcsolódásakor: ' + err);
+    } else {
+        console.log('Sikeres csatlakozás a MySQL adatbázishoz.');
+        connection.release();
+    }
+});
 
+// Socket.IO kapcsolat kezelése
 io.on('connection', (socket) => {
     console.log('Egy felhasználó csatlakozott:', socket.id);
 
-    // Kérdés küldése a felhasználónak
-    socket.emit('newQuestion', 'Mi Magyarország fővárosa?');
+    // Véletlenszerű kérdés lekérése az adatbázisból
+    pool.query('SELECT * FROM questions ORDER BY RAND() LIMIT 1', (err, results) => {
+        if (err) {
+            console.error('Hiba a kérdés lekérésekor:', err);
+            socket.emit('newQuestion', 'Hiba történt a kérdés betöltése közben.');
+            return;
+        }
 
-    // Válasz fogadása
-    socket.on('submitAnswer', (answer) => {
-        console.log(`${socket.id} válasza: ${answer}`);
-        
-        // Válasz ellenőrzése
-        if (answer.toLowerCase() === "vau") {
-            socket.emit('answerFeedback', 'Helyes válasz!');
+        if (results.length > 0) {
+            // Ha van kérdés, elküldjük
+            const question = results[0].question;
+            socket.emit('newQuestion', question);  // Küldjük a kérdést a kliensnek
         } else {
-            socket.emit('answerFeedback', 'Helytelen válasz, próbáld újra!');
+            socket.emit('newQuestion', 'Nincs elérhető kérdés.');
         }
     });
 
@@ -33,4 +57,10 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('Egy felhasználó kilépett:', socket.id);
     });
+});
+
+// Szerver indítása
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
 });
--- server.js
+++ server.js
@@ -1,45 +1,98 @@
 require('dotenv').config();
 const express = require('express');
-
 const http = require('http');
 const socketio = require('socket.io');
 const ejs = require('ejs');
+const mysql = require('mysql');
+const session = require('express-session');
 
+// Alap beállítások
 const app = express();
-var session = require('express-session');
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const coreRoutes = require('./Modules/core');
 app.use('/', coreRoutes);
 
+// Szerver és Socket.IO beállítások
 const server = http.createServer(app);
 const io = socketio(server);
 
-const port = process.env.PORT;
-const { users, rooms, userJoin, userLeave, getRoomUsers, getCurrentUser, inRoomsList, roomLeave } = require('./utils');
+// Adatbázis kapcsolódás
+const pool = mysql.createPool({
+    connectionLimit: process.env.CONNECTIONLIMIT,
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME
+});
+
+// Csatlakozás ellenőrzése
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.log('Hiba a MySQL kapcsolódásakor: ' + err);
+    } else {
+        console.log('Sikeres csatlakozás a MySQL adatbázishoz.');
+        connection.release();
+    }
+});
+
+// Stílusok és statikus fájlok
 app.use('/Assets', express.static('Assets'));
 
+// Session kezelés (ha szükséges)
+app.use(session({
+    secret: 'secret-key', // Titkos kulcs
+    resave: false,
+    saveUninitialized: true,
+}));
 
-app.get('/', (req, res)=>{
+// EJS renderelés
+app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-app.get('/game/:room/:user', (req, res)=>{
-    // let { name, room } = req.body;
+app.get('/game/:room/:user', (req, res) => {
     session.user = req.params.user;
     session.room = req.params.room;
-    res.render('game.ejs', {user:session.user, room:session.room});
+    res.render('game.ejs', { user: session.user, room: session.room });
 });
-app.get('/board', (req, res)=>{
-    // let { name, room } = req.body;
+
+app.get('/board', (req, res) => {
     session.user = req.params.user;
     session.room = req.params.room;
-    res.render('board.ejs', {user:session.user, room:session.room});
+    res.render('board.ejs', { user: session.user, room: session.room });
 });
 
+// Socket.IO kapcsolat kezelése
+io.on('connection', (socket) => {
+    console.log('Egy felhasználó csatlakozott:', socket.id);
 
+    // Véletlenszerű kérdés lekérése az adatbázisból
+    pool.query('SELECT * FROM questions ORDER BY RAND() LIMIT 1', (err, results) => {
+        if (err) {
+            console.error('Hiba a kérdés lekérésekor:', err);
+            socket.emit('newQuestion', 'Hiba történt a kérdés betöltése közben.');
+            return;
+        }
 
-server.listen(port, ()=>{
+        if (results.length > 0) {
+            // Ha van kérdés, elküldjük
+            const question = results[0].question;
+            socket.emit('newQuestion', question);
+        } else {
+            socket.emit('newQuestion', 'Nincs elérhető kérdés.');
+        }
+    });
+
+    // Kilépés a kvízből
+    socket.on('disconnect', () => {
+        console.log('Egy felhasználó kilépett:', socket.id);
+    });
+});
+
+// Szerver indítása
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
 });
